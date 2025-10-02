589f672482c1e7aad13b6538858c7b3d6656513d
Tristan Elizalde <tristane@umich.edu>
2025-10-02 17:02:02 -0400

    Update Hello.java for Jenkins build test

--- Hello.java
+++ Hello.java
@@ -1,7 +1,7 @@
 public class Hello {
     public static void main(String[] args) {
         for (int i = 1; i <= 10; i++) {
-            System.out.println("Updated Hello World..." + i);
+            System.out.println("Jenkins Build Test..." + i);
         }
     }
 }
